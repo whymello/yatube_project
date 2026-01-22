35c77da2beebec923248dfc24316e2672e79b1dc
Azat <azatshat@gmail.com>
2026-01-22 18:56:09 +0300

    Подготовка к тестовому деплою.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -37,6 +38,7 @@ handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
